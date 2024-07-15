395ceae32752270f6e29b452711d1d91c14ad1e9
lincolnwallace1 <lincolnwallace10@gmail.com>
2024-07-15 15:13:55 -0300

    cache manager

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,28 @@
 import { Module } from '@nestjs/common';
 import { ScheduleModule } from '@nestjs/schedule';
+import { CacheInterceptor, CacheModule } from '@nestjs/cache-manager';
 
 import DatabaseConfig from '@common/infra/typeorm/index';
 
 import { UserModule } from '@controller/user/UserModule';
 import { AuthModule } from '@controller/auth/AuthModule';
 import { InvestmentModule } from '@controller/investment/InvestmentModule';
-
 @Module({
   imports: [
     DatabaseConfig,
     ScheduleModule.forRoot(),
+    CacheModule.register({
+      isGlobal: true,
+    }),
     AuthModule,
     UserModule,
     InvestmentModule,
+  ],
+  providers: [
+    {
+      provide: 'APP_INTERCEPTOR',
+      useClass: CacheInterceptor,
+    },
   ],
 })
 export class AppModule {}
